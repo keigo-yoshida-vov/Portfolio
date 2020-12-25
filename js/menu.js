8c8bc0364ae8b30d0e3fde4429ab9c26ecb7a70d
keigo-yoshida-vov <yoshidak51214@gmail.com>
2020-12-26 03:55:45 +0900

    commit

--- js/menu.js
+++ js/menu.js
@@ -6,6 +6,9 @@ $(function() {
         $('#hand_p').animate({
             opacity: "0",
         }, 0, 'none');
+        $('#hand_s').animate({
+            opacity: "0",
+        }, 0, 'none');
         $('#hand_i').animate({
             opacity: "0",
         }, 0, 'none');
@@ -19,6 +22,27 @@ $(function() {
             opacity: "1"
         }, 0, 'none');
         $('.cat').animate({
+            opacity: "0",
+        }, 0, 'none');
+        $('#hand_s').animate({
+            opacity: "0",
+        }, 0, 'none');
+        $('#hand_i').animate({
+            opacity: "0",
+        }, 0, 'none');
+        $('#hand_l').animate({
+            opacity: "0",
+        }, 0, 'none');
+    });
+
+    $('.skills').on('click', function(){
+        $('#hand_s').animate({
+            opacity: "1"
+        }, 0, 'none');
+        $('.cat').animate({
+            opacity: "0",
+        }, 0, 'none');
+        $('#hand_p').animate({
             opacity: "0",
         }, 0, 'none');
         $('#hand_i').animate({
@@ -39,6 +63,9 @@ $(function() {
         $('#hand_p').animate({
             opacity: "0",
         }, 0, 'none');
+        $('#hand_s').animate({
+            opacity: "0",
+        }, 0, 'none');
         $('#hand_l').animate({
             opacity: "0",
         }, 0, 'none');
@@ -52,6 +79,9 @@ $(function() {
             opacity: "0",
         }, 0, 'none');
         $('#hand_p').animate({
+            opacity: "0",
+        }, 0, 'none');
+        $('#hand_s').animate({
             opacity: "0",
         }, 0, 'none');
         $('#hand_i').animate({
@@ -70,4 +100,4 @@ $(function(){
         "scroll-top": position }, speed, "swing");
       return false;
     });
-  });
+});
